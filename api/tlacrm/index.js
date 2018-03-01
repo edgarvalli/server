81d1c89afa731e20364d660e0ef6b96d79220355
edgarvalli <edgarvalli80@gmail.com>
2018-02-28 19:15:08 -0600

    Added home route 2

--- api/tlacrm/index.js
+++ api/tlacrm/index.js
@@ -3,10 +3,11 @@ const clientRouter = require("./clients/clients.route");
 const leadRouter = require("./leads/leads.route");
 const jobRouter = require("./jobs/jobs.route");
 const home = require("./home");
+const { isAuth } = require("../../lib/func");
 
 module.exports = app => {
     app
-        .get("/api/tlacrm/count-records", home)
+        .get("/api/tlacrm/count-records",isAuth, home)
         .use('/api/tlacrm/leads', leadRouter)
         .use('/api/tlacrm/clients', clientRouter)
         .use('/api/tlacrm/auth', authRouter)
