eb01fc3d45e133cd19c8fc6c2c580219be9fd0af
edgarvalli <edgarvalli80@gmail.com>
2018-04-03 11:28:02 -0500

    Remove ssl from server.js

--- server.js
+++ server.js
@@ -4,18 +4,18 @@ const app = express();
 
 /***************** Express setup ******************************/
 
-const options = {
-    key: fs.readFileSync("certificates/ev-server.key"),
-    cert: fs.readFileSync("certificates/ev-server.crt"),
-    ca: fs.readFileSync("certificates/ev-server.csr"),
-};
+// const options = {
+//     key: fs.readFileSync("certificates/ev-server.key"),
+//     cert: fs.readFileSync("certificates/ev-server.crt"),
+//     ca: fs.readFileSync("certificates/ev-server.csr"),
+// };
 
-const https = require("https").Server(options, app);
+// const https = require("https").Server(options, app);
 const http = require("http").Server(app);
 const bodyParser = require('body-parser');
 
 const PORT = 3080;
-const PORTSSL = 3443;
+// const PORTSSL = 3443;
 
 const headers = require('./lib/headers');
 app
@@ -46,5 +46,5 @@ app.get("/mongo", async (req,res) => {
 
 http.listen(PORT , err => err ? console.log(err)
 		: console.log("Server running on port " + PORT));
-https.listen(PORTSSL, err => err ? console.log(err)
-                : console.log("Server running on port secure " + PORTSSL));
+// https.listen(PORTSSL, err => err ? console.log(err)
+//                 : console.log("Server running on port secure " + PORTSSL));
