2644eabb723e29cd3c1329883b082d9a46a01668
Edgar Valli <edgarvalli80@gmail.com>
2018-06-02 21:14:48 -0500

    Modified manage_user

--- manager_users.js
+++ manager_users.js
@@ -12,18 +12,23 @@ rl.question("Display name?: ", name => {
     rl.question("Username?: ", user => {
         rl.question("Password?: ", pass => {
             bcrypt.genSalt(10, (error, salt) => {
-                bcrypt.hash(pass, salt, (error, hash)=> {
+                bcrypt.hash(pass, salt, async (error, hash)=> {
                     const today = { date: now().date, time: now().time };
                     const userInfo = {
                         name,
                         username: user.toLocaleLowerCase(),
                         password: hash,
-                        create_date: today
+                        create_date: today,
+                        avatar: "default.png"   
                     }
-                    mongo("users").insert(userInfo, err => {
-                        err ? console.log(err) : console.log("User saved")
-                        rl.close();
-                    })
+
+                    const users = await mongo.collection("users");
+                    const add = await users.insert(userInfo);
+                    if(add.result.ok === 1){
+                        process.exit();
+                    } else {
+                        console.add(add)
+                    };
                 })
             })
         })
