13e36e9be9cfd29b2b6ff9eb36a634d2ac7a4fe4
Edgar Valli <edgarvalli80@gmail.com>
2018-09-03 22:15:21 -0500

    v2

--- api/users/users.methods.js
+++ api/users/users.methods.js
@@ -44,9 +44,38 @@ module.exports = {
 
     async login(req, res) {
         const { persistent = false, username, password } = req.body;
+        const { client } = req.headers;
         console.log(req.headers)
-        res.json({error: true, msg: 'Error'})
+        const userRequest = username.toLowerCase();
+        const users = await mongo.collection("users");
+        const user = await users.find({username: userRequest}).toArray();
         
+        if(user.length <= 0 || user === undefined) return res.json({error: true, msg:"Usuario no encontrado"})
+        
+        bcrypt.compare(password, user[0].password, (err, success) => {
+            
+            if(err) return res.json({error: true, msg: "Contraseña incorrecta"})
+            
+            delete user[0].password;
+
+            const skt = generateUniqueId(70);
+
+            const info = {
+                user: user[0],
+                skt,
+                client
+            }
+            let token;
+
+            if(persistent) {
+                info.persistent = true
+                token = createToken(info, 100, "years")
+            } else {
+                info.persistent = false;
+                token = createToken(info, 1, "days")
+            }
+            res.json({error: false, user: info.user, token, skt, msg: "Token enviado"})
+        })
     }
 
 }
