256b37d75087b0873e26dd6ff1124b2509743e5f
Edgar Valli <edgarvalli80@gmail.com>
2018-09-05 23:10:38 +0000

    v1

--- socket/leads/leads-socket.js
+++ socket/leads/leads-socket.js
@@ -1,12 +1,15 @@
-const mongo = require("../../../lib/mongo.client")("tlacrm");
+const mongo = require("../../lib/mongo.client")("tlacrm");
 const db = "leads";
-
-module.exports = io => io.of("/leads-socket").on("connection", socket => {
+const nsp = '/leads-socket';
+module.exports = io => io.of(nsp).on("connection", socket => {
 
     console.log("User connected on leads socket");
 
-    socket.on('test', msg => io.emit(msg));
-    socket.on('new-lead', lead =>  io.to('/leads-socket').emit(lead) );
+    socket.on('test', msg => {
+	console.log(msg);
+	io.of(nsp).emit('test', msg)
+    });
+    socket.on('new-lead', lead =>  io.of('/leads-socket').emit('new-lead',lead) );
 
     socket.on("disconnect", () => console.log(`User disconnected with id: ${socket.id}`))
 
