3826e1e33495974948a411e20eaf9587a150df9e
Edgar Valli <edgarvalli80@gmail.com>
2018-09-06 17:24:27 -0500

    v5.7

--- socket/leads/leads-socket.js
+++ socket/leads/leads-socket.js
@@ -1,21 +1,9 @@
 const leadController = require('./leads-controller');
-const jwt = require('jwt-simple');
-const { secret } = require('../../lib/func');
+const { socketAuthentication } = require('../../lib/func');
 const nsp = '/leads-socket';
 module.exports = io => io.of(nsp).on("connection", socket => {
-    // Part of socket authentication
-
-    // Get token and skt from client
-    const { token, skt } = socket.handshake.query;
-    // Check if token exits
-    if(!token) return socket.disconnect();
-    // Decoded token
-    const decodeToken = jwt.decode(token, secret);
-    //Check if secret key token is the same as the token
-    if(decodeToken.user.skt !== skt) return socket.disconnect();
-    // Set user information in user
-    socket.user = decodeToken.user.user;
-    console.log(socket.user);
+    
+    socketAuthentication(socket);
     
     socket.on('test', msg => {
         console.log(msg);
