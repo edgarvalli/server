d9b1f22f418a00542596b5de9a3a0eea605b4bb6
Edgar Valli <edgarvalli80@gmail.com>
2018-09-06 16:58:27 -0500

    v5.7

--- socket/leads/leads-socket.js
+++ socket/leads/leads-socket.js
@@ -2,42 +2,26 @@ const leadController = require('./leads-controller');
 const jwt = require('jwt-simple');
 const { secret } = require('../../lib/func');
 const nsp = '/leads-socket';
-module.exports = io => {
+module.exports = io => io.on("connection", socket => {
 
-    const _io = io.of(nsp).use((socket, next) => {
-        const { token, skt } = socket.request._query;
-        if(!token){
-            console.log('No token provided');
-            console.log(token);
-            socket.disconnect();
-            return null;
-        }
-        const decodeToken = jwt.decode(token, secret);
-        if(!decodeToken) {
-            console.log('Error to decoded your token');
-            console.log(token)
-            socket.disconnect();
-        }
-        if(decodeToken.skt === skt) {
-            socket.user = decodeToken;
-            next();
-        }
+    const { token, skt } = socket.handshake.query;
+
+    if(!token) return socket.disconnect();
+    const decodeToken = jwt.decode(token, secret);
+    if(decodeToken !== skt) return socket.disconnect();
+    socket.user = decodeToken;
+    console.log(socket.user)
+
+    console.log("User connected on leads socket ID: " + socket.id);
+    
+    socket.on('test', msg => {
+        console.log(msg);
+        io.of(nsp).emit('test', msg)
     });
 
-    _io.on("connection", socket => {
+    socket.on('new-lead', data => leadController(io,socket).newLead(data))
 
-        console.log("User connected on leads socket ID: " + socket.id);
-        
-        socket.on('test', msg => {
-            console.log(msg);
-            io.of(nsp).emit('test', msg)
-        });
-    
-        socket.on('new-lead', data => leadController(io,socket).newLead(data))
-    
-        // Handle disconnect
-        socket.on("disconnect", () => console.log(`User disconnected with id: ${socket.id}`))
-    
-    })    
+    // Handle disconnect
+    socket.on("disconnect", () => console.log(`User disconnected with id: ${socket.id}`))
 
-}
+})
