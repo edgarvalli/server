63287e080a1897debb30ee75537449478d82bc9f
Edgar Valli <edgarvalli80@gmail.com>
2018-09-08 00:13:30 -0500

    v1

--- api/clients/client.controller.js
+++ api/clients/client.controller.js
@@ -134,7 +134,7 @@ module.exports = {
     },
 
     async convertToClient(req, res) {
-        const values = req.body.data;
+        const values = req.body;
         const _id = mongo.id(values._id);
         delete values._id;
         values.update_date = new Date();
