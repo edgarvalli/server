e27c7dc185f51a89e8dfa763dab600e008280680
Edgar Valli <edgarvalli80@gmail.com>
2018-09-23 11:37:50 -0500

    rebuild v2

--- public/tlacrm/sw.js
+++ public/tlacrm/sw.js
@@ -1,15 +1,18 @@
 self.addEventListener('install', function(ev){
-    const api = 'https://ev-server.ddns.net/'
     ev.waitUntil(
         caches.open('tlacrm').then(cache => {
             return cache.addAll(
                 [
-                    api + '/tlacrm/css/styles.css',
-                    api + '/tlacrm/js/bundle.js',
-                    api + '/tlacrm/index.html',
-                    api + '/tlacrm/images/icons/icon_16x16.png'
+                    '/tlacrm/css/styles.css',
+                    '/tlacrm/js/bundle.js',
+                    '/tlacrm/index.html',
+                    '/tlacrm/images/icons/icon_16x16.png'
                 ]
             )
         })
     )
 });
+
+self.addEventListener('fetch', function(ev) {
+    console.log(ev)
+})
