314309bd7f26079512913bca221e9e5149238e07
Edgar Valli <edgarvalli80@gmail.com>
2018-09-29 18:01:17 -0500

    png

--- api/users/users.methods.js
+++ api/users/users.methods.js
@@ -22,7 +22,7 @@ module.exports = {
         const img = image.originalname.split(".");
         if(img[1] !== 'png' && img[1] !== 'jpg') return res.json({error: true, msg: 'Formato de imagen no soportado, solo PNG y JPG'})
         
-        const filename = `${req.client.user._id}.${img[1]}`;
+        const filename = `${req.client.user._id}.png`;
         const oldDest = path.join(__dirname, `../../${image.path}`)
         const pathProfile = "../../public/tlacrm/images/profiles"
         const newDest = path.join(__dirname, `${pathProfile}/${filename}`)
