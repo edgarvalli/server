7d575eb6a8f5af9c26164213d02cfe00578af00d
Edgar Valli <edgarvalli80@gmail.com>
2019-02-08 11:28:28 -0600

    Adding budget module

--- routes/tlacrm/budgets.router.js
+++ routes/tlacrm/budgets.router.js
@@ -5,11 +5,11 @@ const { tokenExpiration } = require("../../middleware");
 
 
 router
-    .post('add', controller.add)
-    .post('update', controller.update)
-    .get('fetch', controller.fetch)
-    .get('getone', controller.getOne)
-    .get('remove', controller.remove)
-    .get('search', controller.search)
+    .post('/add', controller.add)
+    .post('/update', controller.update)
+    .get('/fetch', controller.fetch)
+    .get('/getone', controller.getOne)
+    .get('/remove', controller.remove)
+    .get('/search', controller.search)
 
 module.exports = router;
