e21444e0c1114d5060cc9b33077414252b842488
Edgar Valli <edgarvalli80@gmail.com>
2019-02-09 01:57:50 -0600

    tlacrm add budget module

--- socket/tlacrm.socket.js
+++ socket/tlacrm.socket.js
@@ -3,7 +3,7 @@ const { mongoClient } = require("../helpers");
 module.exports = async (socket) => {
     //const db = await mongoClient("tlacrm").collection(socket.collection).catch(error => console.log(error))
     console.log(socket)
-    socket.on("add_child", data => {
+    socket.on("client_typing", data => {
         console.log(data)
     })
 }
