7fcb7be896bb7e7e3761e7c9f6e2b184e84ece81
Edgar Valli <edgarvalli80@gmail.com>
2019-02-10 23:42:54 -0600

    tlacrm add budget module

--- helpers/config.js
+++ helpers/config.js
@@ -3,8 +3,8 @@ module.exports = {
     headers(_, res, next) {
         res.header('Access-Control-Allow-Origin', '*');
         res.header('Access-Control-Allow-Credentials', true);
-        res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS, DATA');
-        res.header('Access-Control-Allow-Headers', 'Content-Type, Token');
+        res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+        res.header('Access-Control-Allow-Headers', 'Content-Type, Token, DATA');
 
         // Pass to next layer of middleware
         next();
