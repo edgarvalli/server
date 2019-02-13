adfba5c808b4699c2bd6dca9854d6fa2b03dbcfe
Edgar Valli <edgarvalli80@gmail.com>
2019-02-12 23:14:16 -0600

    evbase

--- evbase/evbase.controller.js
+++ evbase/evbase.controller.js
@@ -7,12 +7,12 @@ module.exports = {
         const limit = d.limit || 0;
         const skip = d.skip || 0;
         const sort = d.sort || {};
-        const filter = d.filter || {};
+        const filter = d.filter || { _id: -1 };
         const { dbname, collection } = req.headers;
         const db = await mongo(dbname).collection(collection);
         const children = await db.find(filter).limit(limit).skip(skip).sort(sort).toArray();
         res.json({ error: false, data: { children } })
-        
+
     }
 
 }
