d63b5dd54cc83caa346221238fc02faaf18929af
Edgar Valli <edgarvalli80@gmail.com>
2019-02-14 23:42:12 -0600

    evbase

--- evbase/evbase.controller.js
+++ evbase/evbase.controller.js
@@ -3,15 +3,11 @@ module.exports = {
 
     async fetch(req, res) {
 
-        const d = req.query;
-        const limit = d.limit || 50;
-        const skip = d.skip || 0;
-        const sort = d.sort || { _id: -1 };
-        const filter = d.filter || {};
+        const { limit, skip } = req.params;
         const { dbname, collection } = req.headers;
         const db = await mongo(dbname).collection(collection);
-        const children = await db.find(filter).sort(sort).limit(limit).skip(skip).toArray();
-        res.json({ error: false, data: { children, d } })
+        const children = await db.find().limit(limit || 50).skip(skip || 0).toArray();
+        res.json({ error: false, data: { children } })
 
     }
 
--- evbase/evbase.router.js
+++ evbase/evbase.router.js
@@ -1,21 +1,7 @@
 const router = require('express').Router();
 const evbase = require('./evbase.controller');
 
-const parseParams = (req, _, next) => {
-    if(req.params.q) {
-        const { q } = req.params;
-        const params = {};
-        q.split("&").forEach(el => {
-            console.log(el)
-            const e = el.split('=');
-            params[e[0]] = e[1];
-        })
-        req.query = params
-    }
-    next();
-}
-
 router
-    .get('/fetch/:q?*', parseParams, evbase.fetch)
+    .get('/fetch/:limit?*/skipt?*', parseParams, evbase.fetch)
 
 module.exports = router;
