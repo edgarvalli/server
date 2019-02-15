d7e88e19a0bc2ba90e6a0601fec0137d5e66fc36
Edgar Valli <edgarvalli80@gmail.com>
2019-02-14 23:10:30 -0600

    evbase

--- evbase/evbase.router.js
+++ evbase/evbase.router.js
@@ -3,7 +3,12 @@ const evbase = require('./evbase.controller');
 
 const parseParams = (req, _, next) => {
     const { q } = req.params;
-    const params = q.split("&")
+    const params = {};
+    q.split("&").forEach(el => {
+        el.split("=").forEach(e => {
+            params[e[0]] = e[1]
+        })
+    })
     req.query = params;
     next();
 }
