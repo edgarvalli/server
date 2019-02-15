486314dfa71837778881fef449510505f8f7c459
Edgar Valli <edgarvalli80@gmail.com>
2019-02-14 23:45:56 -0600

    evbase

--- evbase/evbase.router.js
+++ evbase/evbase.router.js
@@ -2,6 +2,6 @@ const router = require('express').Router();
 const evbase = require('./evbase.controller');
 
 router
-    .get('/fetch/:limit?*/skipt?*', evbase.fetch)
+    .get('/fetch/:limit?/skipt?', evbase.fetch)
 
 module.exports = router;
