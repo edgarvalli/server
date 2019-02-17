cb0463bb10de9245554acfc6665c8d642b795a76
Edgar Valli <edgarvalli80@gmail.com>
2019-02-17 12:00:05 -0600

    Push Notification

--- public/tlacrm/serviceworker.tlacrm.js
+++ public/tlacrm/serviceworker.tlacrm.js
@@ -2,16 +2,16 @@ self.addEventListener('install', event => {
     console.log(event);
 })
 self.addEventListener('activate', function (ev) {
-    ev.waitUntil(
-        caches.open('tlacrm').then(cache => {
-            return cache.addAll(
-                [
-                    '/tlacrm/css/styles.css',
-                    '/tlacrm/js/bundle.js',
-                    '/tlacrm/index.html',
-                    '/tlacrm/images/icons/icon_16x16.png'
-                ]
-            )
-        }).catch(error => console.log(error))
-    )
+    // ev.waitUntil(
+    //     caches.open('tlacrm').then(cache => {
+    //         return cache.addAll(
+    //             [
+    //                 '/tlacrm/css/styles.css',
+    //                 '/tlacrm/js/bundle.js',
+    //                 '/tlacrm/index.html',
+    //                 '/tlacrm/images/icons/icon_16x16.png'
+    //             ]
+    //         )
+    //     }).catch(error => console.log(error))
+    // )
 });
