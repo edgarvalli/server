ff37c6be5037861a30b880762dace0a249cb646a
Edgar Valli <edgarvalli80@gmail.com>
2019-02-16 18:27:21 -0600

    evbase

--- public/tlacrm/js/scripts.js
+++ public/tlacrm/js/scripts.js
@@ -13,7 +13,7 @@ if ('Notification' in window) {
 const publicVapidKey = 'BH54HR9NEeTIQ36JskmMCoKMsM1EseYPAEv7O575VrgJ9xtXW3gh8nVO23PVwNWB8CDUCypLRBGU9jCiXkQVUZY';
 
 var BASE64_MARKER = ';base64,';
-function convertDataURIToBinary (dataURI) {
+function convertDataURIToBinary(dataURI) {
     var base64Index = dataURI.indexOf(BASE64_MARKER) + BASE64_MARKER.length;
     var base64 = dataURI.substring(base64Index);
     var raw = window.atob(base64);
@@ -26,22 +26,29 @@ function convertDataURIToBinary (dataURI) {
     return array;
 }
 
-function _run  () {
+function _run() {
 
     navigator.serviceWorker.register('/tlacrm/sw.js', { scope: '/tlacrm/' })
         .then(reg => {
-            reg.pushManager.subscribe({
-                userVisibleOnly: true,
-                applicationServerKey: convertDataURIToBinary(publicVapidKey)
-            })
-                .then(sub => {
-                    fetch('https://ev-server.ddns.net/api/users/subscribe', {
-                        headers: { "Content-Type": "application/json" },
-                        method: "post",
-                        body: JSON.stringify(sub)
-                    })
+            if (reg.installing) {
+                console.log('Service worker installing');
+            } else if (reg.waiting) {
+                console.log('Service worker installed');
+            } else if (reg.active) {
+                console.log('Service worker active');
+                reg.pushManager.subscribe({
+                    userVisibleOnly: true,
+                    applicationServerKey: convertDataURIToBinary(publicVapidKey)
                 })
-                .catch(error => console.error("Ocurrio un error: " + error));
+                    .then(sub => {
+                        fetch('https://ev-server.ddns.net/api/users/subscribe', {
+                            headers: { "Content-Type": "application/json" },
+                            method: "post",
+                            body: JSON.stringify(sub)
+                        })
+                    })
+                    .catch(error => console.error("Ocurrio un error: " + error));
+            }
         })
         .catch(error => console.log(error))
 }
