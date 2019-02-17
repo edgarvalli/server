cb22a06b1e1436c1bf350bd8ba90c881cf92e364
Edgar Valli <edgarvalli80@gmail.com>
2019-02-17 10:38:20 -0600

    Push Notification

--- public/tlacrm/js/scripts.js
+++ public/tlacrm/js/scripts.js
@@ -2,7 +2,7 @@ if ('serviceWorker' in navigator) {
 
     console.log('Registering service worker');
 
-    navigator.serviceWorker.register('sw.tlacrm.js').then(reg => {
+    navigator.serviceWorker.register('sw.js').then(reg => {
         let sw;
         if (reg.installing) sw = reg.installing;
         if (reg.waiting) sw = reg.waiting;
