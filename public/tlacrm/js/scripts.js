60ad519068b3662b49bf4d70fc080d074dad305b
Edgar Valli <edgarvalli80@gmail.com>
2019-02-16 18:24:49 -0600

    evbase

--- public/tlacrm/js/scripts.js
+++ public/tlacrm/js/scripts.js
@@ -1,7 +1,7 @@
 if ('serviceWorker' in navigator) {
     console.log('Registering service worker');
 
-    _run().catch(error => console.error("Ocurrio un error: " + error));
+    _run();
 }
 
 if ('Notification' in window) {
@@ -13,7 +13,7 @@ if ('Notification' in window) {
 const publicVapidKey = 'BH54HR9NEeTIQ36JskmMCoKMsM1EseYPAEv7O575VrgJ9xtXW3gh8nVO23PVwNWB8CDUCypLRBGU9jCiXkQVUZY';
 
 var BASE64_MARKER = ';base64,';
-const convertDataURIToBinary = dataURI => {
+function convertDataURIToBinary (dataURI) {
     var base64Index = dataURI.indexOf(BASE64_MARKER) + BASE64_MARKER.length;
     var base64 = dataURI.substring(base64Index);
     var raw = window.atob(base64);
@@ -24,33 +24,6 @@ const convertDataURIToBinary = dataURI => {
         array[i] = raw.charCodeAt(i);
     }
     return array;
-}
-
-async function run() {
-    console.log('Registering service worker');
-    const reg = await navigator.serviceWorker.
-        register('/tlacrm/sw.js', { scope: '/tlacrm/' });
-    console.log('Registered service worker');
-
-    console.log('Registering push');
-    const subscription = await window.pushManager.
-        subscribe({
-            userVisibleOnly: true,
-            // The `urlBase64ToUint8Array()` function is the same as in
-            // https://www.npmjs.com/package/web-push#using-vapid-key-for-applicationserverkey
-            applicationServerKey: convertDataURIToBinary(publicVapidKey)
-        });
-    console.log('Registered push');
-
-    console.log('Sending push');
-    await fetch('https://ev-server.ddns.net/api/users/subscribe', {
-        method: 'POST',
-        body: JSON.stringify(subscription),
-        headers: {
-            'content-type': 'application/json'
-        }
-    });
-    console.log('Sent push');
 }
 
 function _run  () {
@@ -69,4 +42,5 @@ function _run  () {
                     })
                 })
         })
+        .catch(error => console.error("Ocurrio un error: " + error));
 }
