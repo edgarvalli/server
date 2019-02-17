e040d6684e9fea7e021d52bb5844895f344d50f0
Edgar Valli <edgarvalli80@gmail.com>
2019-02-16 19:33:44 -0600

    evbase

--- scripts/send_notification.js
+++ scripts/send_notification.js
@@ -2,7 +2,7 @@ const webpush = require('web-push');
 const publicVapidKey = "BH54HR9NEeTIQ36JskmMCoKMsM1EseYPAEv7O575VrgJ9xtXW3gh8nVO23PVwNWB8CDUCypLRBGU9jCiXkQVUZY";
 const privateVapidKey  = "ZZjM202Zbg5qeyiIapP1s07xSJcGw9Tpbsn6iSkNNC8";
 
-webpush.setVapidDetails('edgarvalli80@gmail.com', publicVapidKey, privateVapidKey);
+webpush.setVapidDetails('mailto:edgarvalli80@gmail.com', publicVapidKey, privateVapidKey);
 const sub = {
     endpoint:
         'https://fcm.googleapis.com/fcm/send/d6LrQ0CAiBM:APA91bHkuvRahLZUAkrN-xRuIgnjIKinObdbKdX-FtmRsXRwUhkxi-mUgaimpdA2msw161xRBAy57zm_qHHKQzToVJlP5KKYoYnPgRttC6Qxi_6RVh3x9-zoO9_sCgnVCrHWQtkgacIR',
