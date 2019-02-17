7a62e8eb0f300e89d0d448fc4cca750ca97376cc
Edgar Valli <edgarvalli80@gmail.com>
2019-02-16 19:54:51 -0600

    Push Notification

--- routes/tlacrm/users.router.js
+++ routes/tlacrm/users.router.js
@@ -14,6 +14,7 @@ router
     // .get('/m', lead.addNewFields)
     .post('/subscribe', (req, res) => {
         const subscription = req.body;
+        console.log(subscription)
         const payload = {
             title: "TlaCrm Notification",
             message: "Tu te has subscripto a las notificaciones",
