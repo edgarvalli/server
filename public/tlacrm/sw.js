03a1f2077d7b03c7f726e37c5c8b07e84e6c1f54
Edgar Valli <edgarvalli80@gmail.com>
2019-02-18 10:25:39 -0600

    Progressive web app

--- public/tlacrm/sw.js
+++ public/tlacrm/sw.js
@@ -3,11 +3,13 @@ self.addEventListener('install', function (ev) {
         caches.open('tlacrm').then(cache => {
             return cache.addAll(
                 [
-                    'https://ev-server.ddns.net/tlacrm/css/styles.css',
-                    'https://ev-server.ddns.net/tlacrm/js/bundle.js',
-                    'https://ev-server.ddns.net/tlacrm/js/bundle.chunk.js',
-                    'https://ev-server.ddns.net/tlacrm/index.html',
-                    'https://ev-server.ddns.net/tlacrm/images/icons/icon_16x16.png'
+                    '/tlacrm/js/bundle.js',
+                    '/tlacrm/js/bundle.chunk.js',
+                    '/tlacrm/index.html',
+                    '/tlacrm/images/icons/icon_16x16.png',
+                    '/tlacrm/css/styles.css',
+                    '/tlacrm/roboto.css',
+                    '/tlacrm/material-icons.css'
                 ]
             )
         }).catch(error => console.log(error))
