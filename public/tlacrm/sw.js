68ac3b366c78e3bd01c2a1e15409ead72ac176f1
Edgar Valli <edgarvalli80@gmail.com>
2019-02-17 23:10:20 -0600

    Push Notification

--- public/tlacrm/sw.js
+++ public/tlacrm/sw.js
@@ -12,3 +12,14 @@ self.addEventListener('install', function (ev) {
         }).catch(error => console.log(error))
     )
 });
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(cacheNames.filter(cacheName => cacheName != currentCacheName).map(cacheName => {
+                return caches.delete(cacheName);
+            })
+            )
+        })
+    )
+})
