551481cf59a28ff2c8634f6597f3ccb73789bc98
Edgar Valli <edgarvalli80@gmail.com>
2019-06-30 01:20:04 -0500

    avatar

--- routes/tlacrm/index.js
+++ routes/tlacrm/index.js
@@ -1,10 +1,31 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
 router
-    .use("/leads", require('./leads.router'))
-    .use("/clients", require('./clients.router'))
-    .use("/jobs", require('./jobs.router'))
-    .use("/users", require('./users.router'))
-    .use('/budgets', require('./budgets.router'))
+  .use("/leads", require("./leads.router"))
+  .use("/clients", require("./clients.router"))
+  .use("/jobs", require("./jobs.router"))
+  .use("/users", require("./users.router"))
+  .use("/budgets", require("./budgets.router"))
+  .get("/get-avatar/:id", (req, res) => {
+    const { id } = req.params;
+    const fs = require("fs");
+    const path = require("path");
+    const filePng = path.join(
+      path.join(__dirname, "../../files/profiles/" + id + ".png")
+    );
+    const fileJpg = path.join(
+      path.join(__dirname, "../../files/profiles/" + id + "jpg")
+    );
+    const fileDefault = path.join(
+      path.join(__dirname, "../../files/profiles/default_avatar.png")
+    );
+    if (fs.existsSync(filePng)) {
+      res.sendFile(filePng);
+    } else if (fs.existsSync(fileJpg)) {
+      res.sendFile(fileJpg);
+    } else {
+      res.json(fileDefault);
+    }
+  });
 
 module.exports = router;
