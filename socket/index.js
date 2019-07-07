a2e66db492c3ad6bdcedf48186e27acb392b19d6
Edgar Valli <edgarvalli80@gmail.com>
2019-07-07 18:47:31 -0500

    Module Budgets

--- socket/index.js
+++ socket/index.js
@@ -21,7 +21,7 @@ module.exports = app => {
       try {
         comment.createDate = new Date();
         await mongo.Push(id, { comments: comment });
-        socket.broadcast.emit("add_commnet", comment);
+        io.sockets.in(room).emit("add_commnet", comment);
       } catch (message) {
         console.log(message);
       }
